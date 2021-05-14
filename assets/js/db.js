9774102783647c46a658be2948e277d9d72eb5ae
yellowuncle13 <cima103303043@gmail.com>
2021-05-14 23:26:15 +0800

    update new chapter.

--- assets/js/db.js
+++ assets/js/db.js
@@ -19,13 +19,35 @@ var get_start = (function () {
 
     // Click button update db
     $('input:button').click(function(){
-        var name = $('#add-name').val(),
+        var check = true,
+            name = $('#add-name').val(),
             content = $('#add-content').val();
-        
-        if (name != "" && content != "") {
+        if (name == "yellowuncle" || name == "小黃叔") {
+            var value = prompt("你是小黃叔嗎?");
+            db.ref(`/myAccount`).on('value', function (snapshot) {
+                var password = snapshot.val();
+                if (value == password) {
+                    db.ref(`/comments/${chaptNum}`).push({
+                        name : name,
+                        content: content,
+                        status: "",
+                        upvote: 0,
+                        downvote: 0,
+                        time: _DateTimezone(8)
+                    });
+                }
+                else {
+                    alert("換個名字吧。")
+                }
+            });
+        }
+        else if (name != "" && content != "") {
             db.ref(`/comments/${chaptNum}`).push({
                 name : name,
                 content: content,
+                status: "",
+                upvote: 0,
+                downvote: 0,
                 time: _DateTimezone(8)
             });
         }
@@ -52,17 +74,26 @@ var get_start = (function () {
                 }
                 _createPageStr(len, names, contents, times);
             }
+            else {
+                db.ref(`/cmtCnt/${chaptNum}`).set(0);
+                cmtCnt.innerHTML = 0;
+            }
         });
     }
 
     // update to page
     function _createPageStr(len, names, contents, times) {
         var str = `<div class="container" style="text-align: left;">`;
-        for (let i = 0; i < len; i++) {
+        var i = 0;
+        var style = "";
+        for (i = 0; i < len; i++) {
+            if (`${names[i]}` == `yellowuncle` || `${names[i]}` == `小黃叔`) {
+                style = `badge bg-dark text-wrap`;
+            }
             str += `
                 <div class="card border border-dark">
                     <div class="card-body">
-                        <h5 class="card-title fs-5 fw-bold">${names[i]}</h5>
+                        <h5 class="card-title fs-5 fw-bold ${style}">${names[i]}</h5>
                         <h6 class="card-text fs-5" style="position: absolute; top: 3px; right: 5px; text-align:right;">#${i+1}</h6>
                         <h6 class="card-text text-muted" style="position: absolute; top: 30px; right: 5px; text-align:right; font-size: 10px;">${times[i]}</h6>
                         <hr class="dropdown-divider">
@@ -74,6 +105,8 @@ var get_start = (function () {
         }
         str += `</div>`
         allComments.innerHTML = str;
+        db.ref(`/cmtCnt/${chaptNum}`).set(i);
+        cmtCnt.innerHTML = i;
     }
 
     // time
